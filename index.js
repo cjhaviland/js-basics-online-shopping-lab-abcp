84a30b9a11bf0e764fc8c38a45158db37d805a46
CJ Haviland <cj.haviland@yahoo.com>
2018-01-17 19:22:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,9 +73,9 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-    
-    console.log(`That item is not in your cart`);
   }
+  
+  console.log(`That item is not in your cart`);
   
   return cart;
 }
