a0095c51da61413e8ad5c3f1a6ae99bb75ba359a
CJ Haviland <cj.haviland@yahoo.com>
2018-01-17 18:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ function removeFromCart(item) {
   
   if (cartLen > 0){
     for (var i = 0; i < cartLen; i++){
+      console.log(cart);
       if (item in cart[i]){
         cart.splice(i, 1);
       }
